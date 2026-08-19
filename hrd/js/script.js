d3957d2be467b928cc9cd78a162ec4058c107f62
ZufarMaliki <zufarmaliki10@gmail.com>
2026-08-19 13:47:45 +0700

    halaman hrd selesai 50%

---
+++ hrd/js/script.js
@@ -0,0 +1,6 @@
+const toggler = document.querySelector(".toggler-btn");
+
+toggler.addEventListener("click", function(){
+    document.querySelector("#sidebar").classList.toggle("collapsed");
+    document.querySelector(".main").classList.toggle("expanded");
+});
